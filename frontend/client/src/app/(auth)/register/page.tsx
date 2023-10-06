e29b2e54953ccf2b6da6c193bfd8e89b4b2ea856
rajahwu <vmradford@gmail.com>
2023-10-06 13:11:24 -0400

    add links to login pages

--- frontend/client/src/app/(auth)/register/page.tsx
+++ frontend/client/src/app/(auth)/register/page.tsx
@@ -1,9 +1,11 @@
-import LoginForm from '/src/components/LoginForm'
+import Link from "next/link";
+import { LoginForm } from "@/components";
 
 export default function Page() {
     const isRegistered = true;
     return (
         <div>
+            <Link href="/dashboard">Dashboard</Link>
             <LoginForm isRegistered={isRegistered} />
         </div>    
     )
--- frontend/client/src/app/dashboard/page.tsx
+++ frontend/client/src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-    return <h1>Dashboard</h1>
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <Link href="/login">Login</Link>
+      <Link href="/register">Sign Up</Link>
+    </>
+  );
 }
