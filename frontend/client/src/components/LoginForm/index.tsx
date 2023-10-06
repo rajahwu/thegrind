da8b9e02d835e663b2afd1fff40097e6adc7f3e4
rajahwu <vmradford@gmail.com>
2023-10-06 16:02:43 -0400

    mark task complete on click

--- frontend/client/src/components/LoginForm/index.tsx
+++ frontend/client/src/components/LoginForm/index.tsx
@@ -1,8 +1,7 @@
 "use-client";
+import Link from "next/link";
 
-export default function LoginForm({ isRegistered }) {
-  const loginType: string = isRegistered === false ? "register" : "login";
-
+export default function LoginForm({ isRegistered }: { isRegistered: boolean }) {
   return (
     <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
       <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
@@ -98,9 +97,8 @@ export default function LoginForm({ isRegistered }) {
         </form>
       </div>
       <div className="flex items-center justify-center mt-6">
-        <a
+        <Link
           href={isRegistered ? "/register" : "/login"}
-          target="_blank"
           className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
         >
             {isRegistered ?
@@ -108,7 +106,7 @@ export default function LoginForm({ isRegistered }) {
             : 
             <span className="ml-2">Already have an account?</span>
         }
-        </a>
+        </Link>
       </div>
     </div>
   );
