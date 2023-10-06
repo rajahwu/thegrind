c551b12e9609c5bd00f8410439ac676f2609dd57
rajahwu <vmradford@gmail.com>
2023-10-06 15:03:50 -0400

    add task card component

--- frontend/client/src/app/dashboard/page.tsx
+++ frontend/client/src/app/dashboard/page.tsx
@@ -1,4 +1,31 @@
+import { type TaskData } from "../../../global.types";
+import TasksCard from "@/components/TasksCard";
 import Link from "next/link";
+
+
+const tasks: TaskData[] = [
+  {
+    title: "Complete assignment",
+    isComplete: false,
+  },
+  {
+    title: "Read a book",
+    isComplete: true,
+  },
+  {
+    title: "Go for a run",
+    isComplete: false,
+  },
+  {
+    title: "Buy groceries",
+    isComplete: true,
+  },
+  {
+    title: "Write code",
+    isComplete: false,
+  },
+];
+
 
 export default function Page() {
   return (
@@ -6,6 +33,9 @@ export default function Page() {
       <h1>Dashboard</h1>
       <Link href="/login">Login</Link>
       <Link href="/register">Sign Up</Link>
+      <div>
+      <TasksCard tasks={tasks} />
+      </div>
     </>
   );
 }
