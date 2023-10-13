1d4ae864b452734d098df8ff00150403bf7b9f8f
rajahwu <vmradford@gmail.com>
2023-10-13 10:19:59 -0400

    add types to user model

--- backend/server/models/user.ts
+++ backend/server/models/user.ts
@@ -1,40 +1,66 @@
-import {
-    Sequelize,
-    DataTypes,
-} from 'sequelize';
+import { Sequelize, Model, DataTypes, Optional } from "sequelize";
 
-export interface UserAttributes {
-    id ? : string;
-    username ? : string;
-    email ? : string;
-    password ? : string;
-
+interface UserAttributes {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
 }
 
-export interface UserInstance {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
+// We use "Optional" to mark the fields that are optional when calling UserModel.create() or UserModel.build()
+interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
 
-    username: string;
-    email: string;
-    password: string;
+class User
+  extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes
+{
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
 
+  // Timestamps created by Sequelize
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
-const User = (sequelize: Sequelize, DataTypes: DataTypes) => {
-    var User = sequelize.define('User', {
-        id: DataTypes.UUID,
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
-    });
+export function initializeUserModel(sequelize: Sequelize) {
+  User.init(
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users", // You can specify the table name here if needed
+      timestamps: true, // Include timestamps (createdAt, updatedAt)
+    }
+  );
+  // Define the associations in a separate function
 
-    User.associate = function(models) {
-        // associations can be defined here
-    };
+  // Define any associations here, similar to the `UserModel` example you provided
 
-    return User;
-};
+  //   export function associateUserModel() {
+  //       // Define associations here
+  //       User.hasMany(/* Another Model */, { foreignKey: 'userId' });
+  //       // Add more associations as needed
+  //   }
+}
 
-export default User
+export default User;
